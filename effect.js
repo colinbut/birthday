04dc13ae983e830cb5fecd10cecdd084ff85c2ca
Colin But <colinbut@users.noreply.github.com>
2018-05-03 09:42:37 +0100

    Changing animation speed and fixing first text message

--- effect.js
+++ effect.js
@@ -142,11 +142,11 @@ $('document').ready(function(){
 		function msgLoop (i) {
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+			$("p:nth-child("+i+")").fadeIn('slow').delay(1500);
 			if(i==45){
 				$("p:nth-child(44)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
-					$('#present').fadeIn('fast');
+					$('#present').fadeIn('slow');
 				});
 				
 			}
